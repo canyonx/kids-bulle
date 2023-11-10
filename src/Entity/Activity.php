c04aac360443c17369fb261f8ccfe7cc4ab2de20
Mattéo <lekokeliko@gmail.com>
2023-11-10 11:22:50 +0100

    first migration

---
+++ src/Entity/Activity.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActivityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ActivityRepository::class)
+ */
+class Activity
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $dateAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateAt(): ?\DateTimeImmutable
+    {
+        return $this->dateAt;
+    }
+
+    public function setDateAt(\DateTimeImmutable $dateAt): self
+    {
+        $this->dateAt = $dateAt;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Child.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChildRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ChildRepository::class)
+ */
+class Child
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $birthAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getBirthAt(): ?\DateTimeImmutable
+    {
+        return $this->birthAt;
+    }
+
+    public function setBirthAt(\DateTimeImmutable $birthAt): self
+    {
+        $this->birthAt = $birthAt;
+
+        return $this;
+    }
+}
