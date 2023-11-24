ab023da9db3aacbb9b2b656ac896ce2dd8988c1b
Mattéo <lekokeliko@gmail.com>
2023-11-24 18:19:28 +0100

    why uxTurbo not work ?

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
-    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
